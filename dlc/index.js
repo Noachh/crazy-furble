e17cfb20acd57d301f3a3ba829c13f09cd4b6fdc
Noachh <31162503+Noachh@users.noreply.github.com>
2024-07-23 03:23:46 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,33 +1,9 @@
 [
     { 
-        "file": "context.dlc", 
-        "title": "Context Logo",
+        "file": "german.dlc", 
+        "title": "German",
         "buttons": [
             { "title": "Logo", "action": [75,0,4,4] }
         ]
     },
-    { 
-        "file": "hacked.dlc", 
-        "title": "HACKED",
-        "buttons": [
-            { "title": "Hacked 1", "action": [75,0,3,4] },
-            { "title": "Hacked 2", "action": [75,0,4,4] }
-        ]
-    },
-    {
-        "file": "german.dlc", 
-        "title": "German",
-        "buttons": [
-            { "title": "Speech", "action": [75,0,1,1] },
-            { "title": "Speech2", "action": [75,0,4,4] },
-            { "title": "Speech3", "action": [75,0,3,4] },
-        ]
-    },
-    {
-        "file": "chilli.dlc", 
-        "title": "Chilli",
-        "buttons": [
-            { "title": "Chilli ", "action": [75,0,4,4] }
-        ]
-    }
 ]
