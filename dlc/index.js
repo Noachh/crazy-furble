2d58c9139875a538509ef1a5f2b54341e2e61ab0
Noachh <31162503+Noachh@users.noreply.github.com>
2024-07-23 02:59:44 +0200

    Update index.js

--- dlc/index.js
+++ dlc/index.js
@@ -1,8 +1,9 @@
 [
     { 
-        "file": "german.dlc", 
-        "title": "German Speech[
-            { "title": "German", "action": [75,0,4,4] }
+        "file": "context.dlc", 
+        "title": "Context Logo",
+        "buttons": [
+            { "title": "Logo", "action": [75,0,4,4] }
         ]
     },
     { 
